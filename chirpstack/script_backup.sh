a8c8d54422dbb1d8df0d30d4d32cab74bf6669df
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2024-12-12 15:55:47 +0100

    Update script_backup.sh

--- chirpstack/script_backup.sh
+++ chirpstack/script_backup.sh
@@ -9,10 +9,10 @@ else
     exit 1
 fi
 
-# Récuperation de la base de données PostgreSQL vers dump.sql
+# Récuperation de la base de données Postgres vers dump.sql
 docker exec -it chirpstack-postgres-1 pg_dumpall -c --no-password -h localhost -U postgres > dump.sql
 
-# Vérification du code de retour
+# Vérification du code de retour de la commande docker
 if [ $? -eq 0 ]; then
     echo "Fichier dump.sql copié dans le dossier actuel."
 else
