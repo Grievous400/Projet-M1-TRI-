c2dbf9d43246b2f1f208781d6bc472e149e9928e
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2024-12-12 15:33:34 +0100

    Create script_backup.sh
    Première version du script de backup

---
+++ chirpstack/script_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copier le fichier dump.rdb de Redis
+if [ -f redisdata/dump.rdb ]; then
+    cp redisdata/dump.rdb dump.rdb
+    echo "Fichier dump.rdb copié dans le dossier actuel."
+else
+    echo "Erreur : Le fichier dump.rdb n'existe pas."
+    exit 1
+fi
+
+# Récuperation de la base de données PostgreSQL vers dump.sql
+sudo docker exec -it chirpstack-postgres-1 pg_dumpall -c --no-password -h localhost -U postgres > dump.sql
