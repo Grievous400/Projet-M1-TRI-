3a660bc2dc90cfe2f53fdc90259ebe897373a866
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2025-01-15 12:51:29 +0100

    Update script_backup.sh

--- chirpstack/script_backup.sh
+++ chirpstack/script_backup.sh
@@ -12,17 +12,17 @@ fi
 
 # Copie le fichier dump.rdb de Redis
 if [ -f redisdata/dump.rdb ]; then
-    cp redisdata/dump.rdb dump.rdb
+    cp redisdata/dump.rdb sauvegardes/dump.rdb
     echo "Fichier dump.rdb copié dans le dossier actuel."
     #Permet au synology de copier le fichier (par défaut : 600 root)
-    chmod 644 dump.rdb
+    chmod 644 sauvegardes/dump.rdb
 else
     echo "Erreur : Le fichier dump.rdb n'existe pas."
     exit 1
 fi
 
 # Récuperation de la base de données Postgres vers dump.sql
-docker exec -it chirpstack-postgres-1 pg_dumpall -c --no-password -h localhost -U postgres > dump.sql
+docker exec -it chirpstack-postgres-1 pg_dumpall -c --no-password -h localhost -U postgres > sauvegardes/dump.sql
 
 # Vérification du code de retour de la commande docker
 if [ $? -eq 0 ]; then
