a0d170df97d902ade0706d20bea60571919374a6
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2025-01-23 18:21:10 +0100

    Update script_backup.sh
    Changement de lieu de stockage des sauvegardes (/var/backup)

--- chirpstack/script_backup.sh
+++ chirpstack/script_backup.sh
@@ -6,10 +6,12 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # Défini une variable pour le chemin d'acces au serveur
 SERVER_DIR="/home/almalinux/thomas/chirpstack"
 
+SAVE_DIR="/var/backup/chirpstack"
+
 # Vérifie si le dossier "sauvegardes" existe
-if [ ! -d "$SERVER_DIR/sauvegardes" ]; then
+if [ ! -d "$SAVE_DIR" ]; then
     # Si le dossier n'existe pas, le créer
-    mkdir "$SERVER_DIR/sauvegardes"
+    mkdir "$SAVE_DIR"
     echo "Le dossier 'sauvegardes' a été créé."
 else
     # Si le dossier existe déjà, ne rien faire
@@ -18,20 +20,20 @@ fi
 
 # Copie le fichier dump.rdb de Redis
 if [ -f "$SERVER_DIR/redisdata/dump.rdb" ]; then
-    cp "$SERVER_DIR/redisdata/dump.rdb" "$SERVER_DIR/sauvegardes/dump.rdb"
+    cp "$SERVER_DIR/redisdata/dump.rdb" "$AVE_DIR/dump.rdb"
     echo "Fichier dump.rdb copié dans le dossier actuel."
     # Permet au Synology de copier le fichier (par défaut : 600 root)
-    chmod 644 "$SERVER_DIR/sauvegardes/dump.rdb"
+    chmod 644 "$SAVE_DIR/dump.rdb"
 else
     echo "Erreur : Le fichier dump.rdb n'existe pas."
     exit 1
 fi
 
 # Changement de dossier pour celui du docker compose afin d'executer la commande docker
-cd /home/almalinux/thomas/chirpstack || exit 1
+cd "$SERVER_DIR" || exit 1
 
 # Récupération de la base de données Postgres vers dump.sql
-/usr/bin/docker exec chirpstack-postgres-1 pg_dumpall -c --no-password -h localhost -U postgres > "$SERVER_DIR/sauvegardes/dump.sql"
+/usr/bin/docker exec chirpstack-postgres-1 pg_dumpall -c --no-password -h localhost -U postgres > "$SAVE_DIR/dump.sql"
 
 # Vérification du code de retour de la commande docker
 if [ $? -eq 0 ]; then
