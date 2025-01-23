c6ae34379885dfeef1ab52ff5278c5d6c015303b
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2025-01-23 18:27:35 +0100

    Update script_restore.sh
    Changement de dossier pour les sauvegardes (/var/backup/chirpstack)

--- chirpstack/script_restore.sh
+++ chirpstack/script_restore.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Défini une variable pour le chemin d'acces au serveur
+SERVER_DIR="/home/almalinux/thomas/chirpstack"
+
+# Défini une variable pour le chemin d'acces a la sauvegarde
+SAVE_DIR="/var/backup/chirpstack"
+
 # Crée les répertoires redisdata et postgresqldata
-mkdir -p redisdata postgresqldata
+mkdir -p "$SERVER_DIR/redisdata" "$SERVER_DIR/postgresqldata"
 
 # Copie le fichier dump.rdb (Redis) dans son répertoire
-if [ -f "sauvegardes/dump.rdb" ]; then
-    cp "sauvegardes/dump.rdb" redisdata/
+if [ -f "$SAVE_DIR/dump.rdb" ]; then
+    cp "$SAVE_DIR/dump.rdb" "$SERVER_DIR/redisdata"
     echo "Fichier dump.rdb copié dans le dossier redisdata."
 else
     echo "Erreur : Le fichier dump.rdb n'existe pas."
@@ -13,12 +19,14 @@ else
 fi
 
 # Vérification de la présence du fichier dump.sql (PostgreSQL)
-if [ -f "sauvegardes/dump.sql" ]; then
+if [ -f "$SAVE_DIR/dump.sql" ]; then
+    # Changement de dossier pour celui du docker compose afin d'executer la commande docker
+    cd "$SERVER_DIR" || exit 1
     # Lance le Docker Compose uniquement pour le service postgres
     docker compose up postgres -d --wait
     sleep 5
     # Restaure la base de données PostgreSQL depuis le dump.sql
-    docker compose exec -T postgres psql -h localhost -U postgres < "sauvegardes/dump.sql"
+    docker compose exec -T postgres psql -h localhost -U postgres < "$SAVE_DIR/dump.sql"
     # Arrête le Docker Compose
     docker compose down
     # Relance le Docker Compose avec tout les services
